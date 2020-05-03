d8fe5ba700e126b3bf192c1e88d8260c33f67072
magenat <1470526115@qq.com>
2020-05-03 13:29:51 +0800

    format

--- article/serializers.py
+++ article/serializers.py
@@ -3,7 +3,7 @@ from abc import ABC
 from rest_framework import serializers
 from rest_framework.pagination import PageNumberPagination
 
-from article.models import Article,Tag,Comment
+from article.models import Article, Tag, Comment
 from tweetme2 import settings
 
 MAX_BLOG_LENGTH = settings.MAX_BLOG_LENGTH
@@ -14,24 +14,27 @@ ARTICLE_ACTION_OPTIONS = settings.TWEET_ACTION_OPTIONS
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ['id','name']
+        fields = ['id', 'name']
 
 
 class CommentSerializer(serializers.ModelSerializer):
     user_name = serializers.CharField(source='user.username')
     user_id = serializers.IntegerField(source='user.id')
+
     class Meta:
         model = Comment
-        fields =['id','content','user_id','user_name','article']
+        fields = ['id', 'content', 'user_id', 'user_name', 'article']
+
 
 class ArticleActionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     action = serializers.CharField()
     content = serializers.CharField(allow_blank=True, required=False)
 
-    def validate_action(self, value):
+    @staticmethod
+    def validate_action(value):
         value = value.lower().strip()
-        if not value in ARTICLE_ACTION_OPTIONS:
+        if value not in ARTICLE_ACTION_OPTIONS:
             raise serializers.ValidationError("NOT A VALID ACTION")
         return value
 
@@ -43,12 +46,14 @@ class ArticleCreateSerializer(serializers.ModelSerializer):
         model = Article
         fields = ['id', 'timestamp', 'content', 'likes_count']
 
-    def validate_content(self, value):
+    @staticmethod
+    def validate_content(value):
         if len(value) > MAX_BLOG_LENGTH:
             raise serializers.ValidationError("长度不能超过140个字")
         return value
 
-    def get_likes_count(self, obj):
+    @staticmethod
+    def get_likes_count(obj):
         return obj.likes.count()
 
 
@@ -72,21 +77,18 @@ class ArticleSerializer(serializers.ModelSerializer):
     likes_count = serializers.SerializerMethodField(read_only=True)
     parent = ArticleParentSerializer(read_only=True)
 
-    tags = TagSerializer(many=True , read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
 
     comments_article = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Article
-        fields = ['id', 'author_name', 'author_id', 'timestamp', 'content', 'likes_count', 'is_retweet', 'parent','tags','comments_article']
-
+        fields = ['id', 'author_name', 'author_id', 'timestamp', 'content', 'likes_count', 'is_retweet', 'parent',
+                  'tags', 'comments_article']
 
     @staticmethod
     def get_likes_count(obj):
         return obj.likes.count()
-
-
-
 
 
 class ArticlePaginations(PageNumberPagination):
@@ -94,5 +96,3 @@ class ArticlePaginations(PageNumberPagination):
     page_query_param = 'page'
     page_size_query_param = 'size'
     max_page_size = 20
-
-
